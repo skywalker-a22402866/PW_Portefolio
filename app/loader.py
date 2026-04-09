0741d95c1ff118cf65862b13fe6e43356ce7cf3f
skywalker-a22402866 <skywalkerfsx@gmail.com>
2026-04-09 16:32:31 +0000

    V9

---
+++ app/loader.py
@@ -0,0 +1,11 @@
+from app.models import *
+import json
+
+with open('app/data/tfcs.json') as f:
+
+    tfcs = json.load(f)
+
+    for tfc in tfcs:
+        print(f"titulo: {tfc['titulo']} Resumo: {tfc['resumo']}")
+        break
+    
